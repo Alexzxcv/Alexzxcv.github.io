cb2a957085744a8ab6ffb54684308fc06ffdd085
Alezxcv <budikov2011@gmail.com>
2022-08-22 21:55:44 +0300

    [add] lesson 10

--- src/js/app.js
+++ src/js/app.js
@@ -7,55 +7,29 @@ let options = {
 };
 
 options.type = prompt("Введите один из вариантов типов сайта \nВизитка, Магазин, Форум");
-// options.design = prompt ("Введите один из вариантов дизайнов сайта \nКлассический, Инфорамционный, Корпоративный");
-// options.adaptability = prompt ("Адаптивность \nДа, Нет");
+options.design = prompt ("Введите один из вариантов дизайнов сайта \nКлассический, Инфорамционный, Корпоративный");
+options.adaptability = prompt ("Адаптивность \nДа, Нет");
 
 let getSum = (obj) => {
 
     let costType = 0;
-    let costDesign = 0;
-    let costAdaptability = 0;
 
     let cost = {
         "Визитка": 10,
         "Магазин": 20,
-        "Форум": 30
+        "Форум": 30,
+
+        "Классический": 10,
+        "Инфорамционный": 20,
+        "Корпоративный": 30,
+
+        "Да": 20,
+        "Нет": 10
     }
 
-    costType = cost[obj.type]
+    costType = cost[obj.type] + cost[obj.design] + cost[obj.adaptability];
 
-    // if (obj.type === 'Визитка') {
-    //     costType = 10;
-    // }
-    // else if (obj.type === 'Магазин'){
-    //     costType = 20;
-    // }
-    // else if (obj.type === 'Форум'){
-    //     costType = 30;
-    // }
-
-    // if (obj.design === 'Классический') {
-    //     costDesign = 10;
-    // }
-    // else if (obj.design === 'Инфорамционный'){
-    //     costDesign = 20;
-    // }
-    // else if (obj.design === 'Корпоративный'){
-    //     costDesign = 30;
-    // }
-
-    // if (obj.adaptability === 'Да') {
-    //     costAdaptability = 20;
-    // }
-    // else if (obj.adaptability === 'Нет'){
-    //     costAdaptability = 10;
-    // }
-
-    // let sum = costType + costDesign + costAdaptability;
-    alert (`Юра лох?\n${costType}`)
+    alert (`Цена вашего сайта:\n${costType}`)
 }
-
-
-
 
 getSum (options);
