abfd272cf042ef1b8890045e83430d62cc62f291
Alezxcv <budikov2011@gmail.com>
2022-08-22 11:15:49 +0300

    add: js

---
+++ src/js/app.js
@@ -0,0 +1,23 @@
+"use strigt"
+
+let options = {
+    tupe: '',
+    design: '',
+    adaptability: ''
+};
+
+options.tupe = prompt("Введите один из вариантов типов сайта \nВизитка, Магазин, Форум");
+// options.design = prompt ("Введите один из вариантов дизайнов сайта \nКлассический, Инфорамционный, Корпоративный");
+// options.adaptability = prompt ("Адаптивность \nДа, Нет");
+
+let createSum = (a) => {
+
+    
+    let sum = this.a;
+    return sum;
+}
+
+let cc = createSum(tupe);
+
+
+alert (`Сумма вашего заказа: \n${cc}`);
